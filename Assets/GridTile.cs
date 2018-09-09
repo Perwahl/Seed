b0b0f1cd5dee1da5b52dcb3b20c1f4b0988e2ac7
Per <per.wahlstedt@gmail.com>
2018-09-09 19:12:19 +0200

    stuff

---
+++ Assets/GridTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile
+{
+    public int gridID;
+    public int triangle1;
+    public int triangle2;
+
+
+}
